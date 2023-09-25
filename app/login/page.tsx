1ec0c78ee8aa54fcd7a951831f51201fa5628741
darrius <mohamedtine17@gmail.com>
2023-09-25 19:21:26 +0000

    to do style for pages login and signup

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,18 +4,20 @@ export  default function Login(){
     return(
         <main>
             <section className={styles.sec1}>
-                <h1>Connexion</h1>
-                <form>
-                    <div>
+                <h1 className={styles.title}>Connexion</h1>
+                <form className={styles.form}>
+                    <div className={styles.min}>
                         <label>Email</label>
                         <input placeholder="Entrer votre adresse mail"/>
                     </div>
 
-                    <div>
+                    <div className={styles.min}>
                         <label>Mot de passe</label>
                         <input placeholder="Entrer votre mot de passe"/>
                     </div>
-                    <input value="Se connecter" />
+                    <button className={styles.log}>
+                        Se connecter
+                    </button>
                 </form>
             </section>
         </main>
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -4,23 +4,25 @@ export  default function Signup(){
     return(
         <main>
             <section className={styles.sec1}>
-                <h1>Connexion</h1>
-                <form>
-                    <div>
+                <h1 className={styles.title}>Creation de compte</h1>
+                <form className={styles.form}>
+                    <div className={styles.min}>
                         <label>Email</label>
                         <input placeholder="Entrer votre adresse mail"/>
                     </div>
 
-                    <div>
+                    <div className={styles.min}>
                         <label>Mot de passe</label>
                         <input placeholder="Donner un mot de passe"/>
                     </div>
 
-                    <div>
+                    <div className={styles.min}>
                         <label>Mot de passe</label>
                         <input placeholder="Repeter le mot de passe"/>
                     </div>
-                    <input value="S'inscrire" />
+                    <button className={styles.log}>
+                        Creer un compte
+                    </button>
                 </form>
             </section>
         </main>
