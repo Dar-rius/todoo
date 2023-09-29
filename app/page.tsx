91e1c6f5ba7e68a175f4391d407d4eead00e53ed
darrius <mohamedtine17@gmail.com>
2023-09-29 02:30:10 +0000

    change position handlesubmit to component client

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import styles from './styles/page.module.css'
+import styles from "./styles/page.module.css";
 import Header from "@/components/header";
 import Forms from "@/components/client/projects";
 import prisma from "@/prisma";
@@ -6,110 +6,98 @@ import Link from "next/link";
 import Image from "next/image";
 import Close from "@/public/close.svg";
 import Check from "@/public/checkmark.svg";
-import React, {use} from 'react'
+import React, { use } from "react";
 
-const getData = async() => {
-  let data = await prisma.project.findMany()
-  return data
-}
-export async function handleSubmit(name:{name:string}){
-  console.log(name)
-  try{
-    const res = await fetch("/api/project", {
-      method: "POST",
-      body: JSON.stringify(name)
-    })
-    if(!res.ok){
-      console.log("Error in request")
-    }
-    console.log(res)
-  } catch(err){
-    console.error(err)
-  }
-}
+const getData = async () => {
+  let data = await prisma.project.findMany();
+  return data;
+};
 
 export default function Home() {
-  let res = use(getData())
+  let res = use(getData());
 
   return (
     <main>
-      <Header/>
+      <Header />
       <section className={styles.sec}>
-        <div style={{
-          display:"flex",
-          justifyContent:"space-around",
-          alignItems:"center",
-          marginBottom:"1.5%",
-        }}>
-          <div  style={{
-            display:"flex",
-            alignItems:"center", }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+            marginBottom: "1.5%",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
             <strong
               style={{
-                color:"#32E6E2",
-                fontSize:"36px",
-                marginRight:"5%"
+                color: "#32E6E2",
+                fontSize: "36px",
+                marginRight: "5%",
               }}
-            >24</strong>
-            <div style={{
-              fontSize:"18px"
-            }}>
-              <strong>
-                Septembre
-              </strong>
-              <div>
-                2023
-              </div>
+            >
+              24
+            </strong>
+            <div
+              style={{
+                fontSize: "18px",
+              }}
+            >
+              <strong>Septembre</strong>
+              <div>2023</div>
             </div>
           </div>
 
-          <div style={{
-            fontSize:"28px"
-          }}>
+          <div
+            style={{
+              fontSize: "28px",
+            }}
+          >
             Lundi
           </div>
         </div>
-        <Forms/>
-        <div style={{
-          display:"flex",
-          flexDirection:"column",
-          justifyContent:"center",
-          marginRight:"24%",
-          marginLeft:"24%",
-        }}>
-          {res.map((item)=>
-              <div className={styles.projects} key={item.id}>
-                <Link href={""}
-                      style={{
-                        fontSize:"18px",
-                        fontWeight:"bold"
-                      }}
-                >{item.name}
-                </Link>
+        <Forms />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            marginRight: "24%",
+            marginLeft: "24%",
+          }}
+        >
+          {res.map((item) => (
+            <div className={styles.projects} key={item.id}>
+              <Link
+                href={""}
+                style={{
+                  fontSize: "18px",
+                  fontWeight: "bold",
+                }}
+              >
+                {item.name}
+              </Link>
 
-                <div style={{
-                  width:"15%",
-                  display:"flex",
-                  justifyContent:"space-between",
-                  alignItems:"center",
-                }}>
-                  <Image
-                      src={Close}
-                      alt={"Fermer"}
-                      width={30}
-                      height={30}
-                  />
-                  <Image
-                      src={Check}
-                      alt={"Finish"}
-                      width={30}
-                      height={30}
-                  />
-                </div>
+              <div
+                style={{
+                  width: "15%",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <Image src={Close} alt={"Fermer"} width={30} height={30} />
+                <Image src={Check} alt={"Finish"} width={30} height={30} />
               </div>
-          )}
+            </div>
+          ))}
         </div>
       </section>
     </main>
-  )
+  );
 }
--- components/client/projects.tsx
+++ components/client/projects.tsx
@@ -1,29 +1,44 @@
-"use client"
-import styles from '@/app/styles/page.module.css'
-import React, {useState} from 'react'
-import {handleSubmit} from "@/app/page";
+"use client";
+import styles from "@/app/styles/page.module.css";
+import React, { useState } from "react";
 
-
-export default function Forms(){
-    let [name, setName] = useState("")
-
-
-    const handleChange = (event: any) =>{
-        setName(event.target.value)
+export async function handleSubmit(name: { name: string }) {
+  console.log(name);
+  try {
+    const res = await fetch("/api/project", {
+      method: "POST",
+      body: JSON.stringify(name),
+    });
+    if (!res.ok) {
+      console.log("Error in request");
     }
+    console.log(res);
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-    return(
-        <form onSubmit={(e : any) =>{
-            e.preventDefault()
-            handleSubmit({name}).then((res)=>
-            console.log(res))
-        }}
-              className={styles.sec2}>
-            <input
-                name="name"
-                placeholder="Ajourter un nouveau projet"
-                onChange={handleChange}/>
-            <button type="submit">+</button>
-        </form>
-    )
+export default function Forms() {
+  let [name, setName] = useState("");
+
+  const handleChange = (event: any) => {
+    setName(event.target.value);
+  };
+
+  return (
+    <form
+      onSubmit={(e: any) => {
+        e.preventDefault();
+        handleSubmit({ name }).then((res) => console.log(res));
+      }}
+      className={styles.sec2}
+    >
+      <input
+        name="name"
+        placeholder="Ajourter un nouveau projet"
+        onChange={handleChange}
+      />
+      <button type="submit">+</button>
+    </form>
+  );
 }
