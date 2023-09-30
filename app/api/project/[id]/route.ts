fc1f8c61af5796ca32356b30cda1c9e7d4950517
darrius <mohamedtine17@gmail.com>
2023-09-30 16:35:20 +0000

    update api project for delete projects and tasks

--- app/api/project/[id]/route.ts
+++ app/api/project/[id]/route.ts
@@ -2,13 +2,24 @@ import { main } from "../route";
 import { type NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma";
 
-export async function DELETE(req: NextRequest, {params}:{params:{id: string}}) {
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } },
+) {
   try {
     const id = Number(params.id);
     await main();
+    await prisma.todo.deleteMany({
+      where: {
+        projectId: id,
+      },
+    });
     await prisma.project.delete({
       where: {
         id: id,
+      },
+      include: {
+        Todo: true,
       },
     });
     return NextResponse.json({ message: "Data deleted" }, { status: 200 });
--- app/todo/[id]/page.tsx
+++ app/todo/[id]/page.tsx
@@ -1,110 +1,125 @@
-import Check from "@/public/checkmark.svg"
+import Check from "@/public/checkmark.svg";
 import Header from "@/components/header";
 import Image from "next/image";
-import styles from "../../styles/todo.module.css"
+import styles from "../../styles/todo.module.css";
 import prisma from "@/prisma";
-import {use} from "react";
+import { use } from "react";
 import AddTodo from "@/components/client/todo/todos";
 import CheckButton from "@/components/client/todo/update";
 
-const getData = async(id: string) => {
-    let _id = Number(id)
-    try{
-        const res = await prisma.project.findUnique({
-          where:{
-              id:_id
-          },
-          include:{
-            Todo:true
-          }
-        })
-        return res
-    }
-    catch(err) {
-        console.error(err)
-    }
-}
+const getData = async (id: string) => {
+  let _id = Number(id);
+  try {
+    const res = await prisma.project.findUnique({
+      where: {
+        id: _id,
+      },
+      include: {
+        Todo: true,
+      },
+    });
+    return res;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-export  default function Todo({params}: {params:{id:string}}){
-    let id = params.id
-    let data = use(getData(id))
-    return(
-        <main>
-           <Header/>
-                    <section className={styles.sec} >
-                        <strong style={{
-                            fontSize:"26px",
-                            display:"flex",
-                            marginLeft:"23%",
-                            marginBottom:"1.5%"
-                        }}>
-                            {data?.name}
-                        </strong>
-                        <AddTodo id={id}/>
-                        <div>
-                            <strong
-                                style={{
-                                    fontSize:"24px",
-                                    display:"flex",
-                                    marginLeft:"23%",
-                                    marginBottom:"1.5%"}}
-                            >
-                                Todo
-                            </strong>
-                            {data?.Todo.map((item)=>
-                                !item.stat &&
-                                <div style={{
-                                    display:"flex",
-                                    flexDirection:"column",
-                                    justifyContent:"center",
-                                    marginRight:"24%",
-                                    marginLeft:"24%",
-                                }} key={item.id}>
-                                    <div className={styles.projects}>
-                                        <p
-                                            style={{
-                                                fontSize:"16px",
-                                            }}>
-                                            {item.content}
-                                        </p>
-                                        <CheckButton id={item.id}/>
-                                    </div>
-                                </div>
-                            )}
-                        </div>
+export default function Todo({ params }: { params: { id: string } }) {
+  let id = params.id;
+  let data = use(getData(id));
+  return (
+    <main>
+      <Header />
+      <section className={styles.sec}>
+        <strong
+          style={{
+            fontSize: "26px",
+            display: "flex",
+            marginLeft: "23%",
+            marginBottom: "1.5%",
+          }}
+        >
+          {data?.name}
+        </strong>
+        <AddTodo id={id} />
+        <div>
+          <strong
+            style={{
+              fontSize: "24px",
+              display: "flex",
+              marginLeft: "23%",
+              marginBottom: "1.5%",
+            }}
+          >
+            Todo
+          </strong>
+          {data?.Todo.map(
+            (item) =>
+              !item.stat && (
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    marginRight: "24%",
+                    marginLeft: "24%",
+                  }}
+                  key={item.id}
+                >
+                  <div className={styles.projects}>
+                    <p
+                      style={{
+                        fontSize: "16px",
+                      }}
+                    >
+                      {item.content}
+                    </p>
+                    <CheckButton id={item.id} />
+                  </div>
+                </div>
+              ),
+          )}
+        </div>
 
-                        <div>
-                            <strong
-                                style={{
-                                    fontSize:"24px",
-                                    display:"flex",
-                                    marginLeft:"23%",
-                                    marginBottom:"1.5%"}}
-                            >
-                                Completed
-                            </strong>
-                            {data?.Todo.map((item)=>
-                                item.stat &&
-                            <div style={{
-                                display:"flex",
-                                flexDirection:"column",
-                                justifyContent:"center",
-                                marginRight:"24%",
-                                marginLeft:"24%",
-                            }}
-                            key={item.id}>
-                                <div className={styles.projectsCompleted}>
-                                    <p style={{
-                                        fontSize:"16px",
-                                        color:"#85929E"
-                                    }}>
-                                        {item.content}
-                                    </p>
-                                </div>
-                            </div>
-                            )}
-                        </div>
-                    </section>
-        </main>
-    )
+        <div>
+          <strong
+            style={{
+              fontSize: "24px",
+              display: "flex",
+              marginLeft: "23%",
+              marginBottom: "1.5%",
+            }}
+          >
+            Completed
+          </strong>
+          {data?.Todo.map(
+            (item) =>
+              item.stat && (
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    marginRight: "24%",
+                    marginLeft: "24%",
+                  }}
+                  key={item.id}
+                >
+                  <div className={styles.projectsCompleted}>
+                    <p
+                      style={{
+                        fontSize: "16px",
+                        color: "#85929E",
+                      }}
+                    >
+                      {item.content}
+                    </p>
+                  </div>
+                </div>
+              ),
+          )}
+        </div>
+      </section>
+    </main>
+  );
 }
